a0e14ce2ad2fcc4a660d4645ccc57c1d5d69813a
Aarush Sen <senaarush@outlook.com>
2024-10-18 03:51:07 +0530

    Refactor authentication routes and add validation middleware
    This commit refactors the authentication routes in the 'auth.ts' file. It replaces the import statement for the authentication controller and adds middleware for request body validation. The validation middleware is imported from the 'validate' module and is used to validate the request body for the 'register' and 'login' routes. Additionally, the commit adds new routes for reading and updating user information, with corresponding validation schemas.

--- routes/auth.ts
+++ routes/auth.ts
@@ -1,10 +1,24 @@
 import express from "express"
 
-import * as controller from "../controllers/authController"
+import * as controller from "../controllers/auth"
+import verifyAuth from "../middlewares/verifyAuth"
+import validate from "../middlewares/validate"
+import {
+    registerSchema,
+    loginSchema,
+    readUserSchema,
+    updateUserSchema
+} from "../validations/auth"
+
+import { z } from "zod"
 
 const router = express.Router()
 
-router.post('/register', controller.register)
-router.post('/login', controller.login)
+
+
+router.post(('/register'), validate(registerSchema), controller.register)
+router.post('/login', validate(loginSchema), controller.login)
+router.get('/users/me', verifyAuth, validate(readUserSchema), controller.readUser)
+router.put('/users/me', verifyAuth, validate(updateUserSchema), controller.updateUser)
 
 export default router
